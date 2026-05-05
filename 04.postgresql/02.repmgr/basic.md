9a725418241dd9575fbc782fa3e7269fb5aa194c
Wenlong <wenldong666@outlook.com>
2026-05-05 19:56:52 +0800

    [Feature] Finish repmgr witness
    Signed-off-by: Wenlong <wenldong666@outlook.com>

--- 04.postgresql/02.repmgr/basic.md
+++ 04.postgresql/02.repmgr/basic.md
@@ -1,12 +1,14 @@
 1. 环境规划
 
-repmgr（监控复制过程、自动故障切换）+ witness（防止脑裂）+ keepalived（监控主库状态，自动切换VIP）
+- repmgr（监控复制过程、自动故障切换）
+- witness（防止脑裂）
+- keepalived（监控主库状态，自动切换VIP）
 
 主库：10.0.0.61  + pg15.5 + postgis3.3 + repmgr5.4.1 [master]
 
 备库：10.0.0.62  + pg15.5 + postgis3.3 + repmgr5.4.1 [slave]
 
-仲裁：10.0.0.63  # todo
+仲裁：10.0.0.63  + pg15.5 + repmgr5.4.1 [witness]
 
 
 2. 安装 repmgr
@@ -15,6 +17,7 @@ repmgr（监控复制过程、自动故障切换）+ witness（防止脑裂）+ 
 
 ```shell
 bash install-pg-15.5-x86.sh
+source /etc/profile
 bash install-postgis-3.3-x86.sh
 bash install-repmgr-master-x86.sh 10.0.0.61 1 primary
 ```
@@ -22,8 +25,9 @@ bash install-repmgr-master-x86.sh 10.0.0.61 1 primary
 
 ```shell
 bash install-pg-15.5-x86.sh
+source /etc/profile
 bash install-postgis-3.3-x86.sh
-bash install-repmgr-slave-x86-prepare.sh 10.0.0.62 2
+bash install-repmgr-slave-prepare-x86.sh 10.0.0.62 2
 ```
 
 61 62 都执行
@@ -39,12 +43,54 @@ exit
 ```
 
 62 备节点执行
+
 ```shell
+# 从 61 节点克隆数据
 sudo -iu postgres repmgr -h 10.0.0.61 -U repmgr -d repmgr -f /data/repmgr/etc/repmgr.conf standby clone --dry-run
 sudo -iu postgres repmgr -h 10.0.0.61 -U repmgr -d repmgr -f /data/repmgr/etc/repmgr.conf standby clone
 # 123456 是 repmgr.conf 中的 password
 
+# 注册为备库
 systemctl start postgresql5432
 sudo -iu postgres repmgr -f /data/repmgr/etc/repmgr.conf standby register --upstream-node-id=1
 sudo -iu postgres repmgr -f /data/repmgr/etc/repmgr.conf cluster show
 ```
+
+3. 安装 witness
+
+63 节点执行
+
+```shell
+bash install-pg-15.5-x86.sh
+source /etc/profile
+bash install-repmgr-witness-prepare-x86.sh 10.0.0.63 3
+
+su postgres
+echo "10.0.0.61:5432:repmgr:repmgr:123456" >> ~/.pgpass
+echo "10.0.0.62:5432:repmgr:repmgr:123456" >> ~/.pgpass
+echo "10.0.0.63:5432:repmgr:repmgr:123456" >> ~/.pgpass
+chmod 600 ~/.pgpass
+cat ~/.pgpass
+exit
+```
+
+61 62执行
+
+```shell
+su postgres
+echo "10.0.0.63:5432:repmgr:repmgr:123456" >> ~/.pgpass
+cat ~/.pgpass
+exit
+```
+
+63 节点执行
+
+```shell
+# 注册节点
+sudo -iu postgres repmgr -f /data/repmgr/etc/repmgr.conf -h 10.0.0.61 -U repmgr -d repmgr witness register
+sudo -iu postgres repmgr -f /data/repmgr/etc/repmgr.conf -U repmgr -d repmgr cluster show
+
+# 查看节点信息
+psql -d repmgr -c "select * from repmgr.nodes;"
+```
+
---
+++ 04.postgresql/02.repmgr/install-repmgr-witness-prepare-x86.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# 色卡
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'  # No Color
+
+# 颜色打印函数
+print_colored() {
+    local color="$1"
+    local message="$2"
+    echo -e "${color}${message}${NC}"
+}
+
+# 校验是否为 root 用户
+if [[ $EUID -ne 0 ]]; then
+   print_colored "$RED" "[Error] This script must be run as root"
+   exit 1
+fi
+
+# 获得 CPU 架构
+arch=$(uname -m)
+if [[ "$arch" == "x86_64" ]]; then
+    print_colored "$GREEN" "[Success] Machine architecture: x86_64"
+elif [[ "$arch" == "aarch64" ]]; then
+    print_colored "$GREEN" "[Success] Machine architecture: aarch64"
+else
+    print_colored "$RED" "[Error] Unsupported machine architecture: $arch"
+    exit 1
+fi
+
+# 使用说明：
+# ./install-repmgr-slave-x86.sh <host_ip> <node_id> 
+# host_ip: 主机IP地址
+# node_id: 节点ID
+# ./install-repmgr-slave-x86.sh 10.0.0.62 2
+
+host_ip=$1
+node_id=$2
+host_port=5432
+
+# host_ip 需要作为传入的参数，如果不传入报错
+if [ -z "$host_ip" ]; then
+    print_colored "$RED" "[Error] host_ip is required"
+    exit 1
+fi
+host_ip=$1
+
+# node_id 需要作为传入的参数，如果不传入报错
+if [ -z "$node_id" ]; then
+    print_colored "$RED" "[Error] node_id is required"
+    exit 1
+fi
+node_id=$2
+
+# 下载 repmgr rpm 包
+# repmgr_15-5.3.3-1.rhel7.x86_64.rpm                 02-Jan-2024 18:05              284208
+# repmgr_15-5.4.0-1.rhel7.x86_64.rpm                 02-Jan-2024 18:05              287604
+# repmgr_15-5.4.1-1PGDG.rhel7.x86_64.rpm             02-Jan-2024 18:05              287784
+# repmgr_15-devel-5.3.3-1.rhel7.x86_64.rpm           02-Jan-2024 18:05                8844
+# repmgr_15-devel-5.4.0-1.rhel7.x86_64.rpm           02-Jan-2024 18:05                9332
+# repmgr_15-devel-5.4.1-1PGDG.rhel7.x86_64.rpm       02-Jan-2024 18:05                9608
+# repmgr_15-llvmjit-5.3.3-1.rhel7.x86_64.rpm         02-Jan-2024 18:05               21736
+# repmgr_15-llvmjit-5.4.0-1.rhel7.x86_64.rpm         02-Jan-2024 18:05               22220
+# repmgr_15-llvmjit-5.4.1-1PGDG.rhel7.x86_64.rpm     02-Jan-2024 18:05               22496
+mkdir -p /usr/local/repmgr5.4.1-rpm
+cd /usr/local/repmgr5.4.1-rpm
+if [[ -f repmgr-5.5.0-1.rhel7.x86_64.rpm ]]; then
+    print_colored "$GREEN" "Repmgr 5.5.0 RPM package already downloaded"
+else
+    print_colored "$YELLOW" "Downloading Repmgr 5.5.0 RPM package..."
+    wget https://download.postgresql.org/pub/repos/yum/15/redhat/rhel-7-x86_64/repmgr_15-5.4.1-1PGDG.rhel7.x86_64.rpm
+fi
+
+rpm -ivh repmgr_15-5.4.1-1PGDG.rhel7.x86_64.rpm
+source /etc/profile
+
+# 创建 repmgr 配置文件目录
+mkdir -p /data/repmgr/etc
+cat > /data/repmgr/etc/repmgr.conf << EOF
+node_id = $node_id
+node_name = node_$node_id
+conninfo = 'host=$host_ip port=$host_port user=repmgr password=123456 dbname=repmgr connect_timeout=2'
+data_directory = '/data/$host_port/data'
+pg_bindir = '/usr/local/pgsql/bin'
+EOF
+chown -R postgres:postgres /data/repmgr
+chmod 700 /data/repmgr
+
+# 创建 repmgr 用户
+psql -c "CREATE USER repmgr WITH SUPERUSER LOGIN PASSWORD '123456';"
+# 创建 repmgr 数据库
+psql -c "CREATE DATABASE repmgr OWNER repmgr;"
+# 更改 repmgr schema
+psql -c "ALTER USER repmgr SET search_path TO repmgr, public;"
+
+# 修改 pg 白名单
+cat >> /data/$host_port/data/pg_hba.conf << EOF
+local repmgr repmgr md5
+host repmgr repmgr 127.0.0.1/32 md5
+host repmgr repmgr 0.0.0.0/0 md5
+
+local replication repmgr md5
+host replication repmgr 127.0.0.1/32 md5
+host replication repmgr 0.0.0.0/0 md5
+EOF
+
+# 重新加载配置
+sudo -iu postgres pg_ctl reload
